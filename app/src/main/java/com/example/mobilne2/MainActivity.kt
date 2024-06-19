d63e466a33bc7b06c20881e7d3c8ddca3efabef1
DusanPajic002 <dusanpajic2002@gmail.com>
2024-06-19 15:13:42 +0200

    9.5

--- app/src/main/java/com/example/mobilne2/MainActivity.kt
+++ app/src/main/java/com/example/mobilne2/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.compositionLocalOf
 import com.example.mobilne2.Acore.theme.Mobilne2Theme
-import com.example.mobilne2.analytics.AppAnalytics
 import com.example.mobilne2.navigation.ScreenManager
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,22 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
 
-    private val analytics = AppAnalytics()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val app = (applicationContext as App)
-
         setContent {
-                Mobilne2Theme {
+            Mobilne2Theme {
 
-                    ScreenManager()
-                }
+                ScreenManager()
+            }
         }
     }
-}
-
-val LocalAnalytics = compositionLocalOf<AppAnalytics> {
-    error("Analytics not provided")
 }
